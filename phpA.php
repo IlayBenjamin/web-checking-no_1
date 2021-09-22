f979183316ec60037505ecd38c2b20c07f82cfa2
IlayBenjamin <91074940+IlayBenjamin@users.noreply.github.com>
2021-09-22 22:58:47 +0300

    df

--- phpA.php
+++ phpA.php
@@ -2,6 +2,7 @@
 #"https://api.github.com/repos/$username/$repo/contents/$path";
 #phpA.php
 #https://ilaybenjamin.github.io/web-checking-no_1/phpA.php
+$myf = fopen("https://api.github.com/repos/IlayBenjamin/web-checking-no_1/contents/web-checking-no_1/xxx.txt")
 $myfilee = fopen("https://ilaybenjamin.github.io/web-checking-no_1/testtfile.txt", "w")
 
 fclose($myfilee);
