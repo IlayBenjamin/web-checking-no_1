d0604ccee57d1a21a7bf55f50b094b0fe2e7bc46
IlayBenjamin <91074940+IlayBenjamin@users.noreply.github.com>
2021-09-22 22:55:44 +0300

    Update phpA

--- phpA.php
+++ phpA.php
@@ -1,6 +1,8 @@
 <?php
-
-$myfilee = fopen("testtfile.txt", "w")
+ "https://api.github.com/repos/$username/$repo/contents/$path";
+#phpA.php
+#https://ilaybenjamin.github.io/web-checking-no_1/phpA.php
+$myfilee = fopen("https://ilaybenjamin.github.io/web-checking-no_1/testtfile.txt", "w")
 
 fclose($myfilee);
 
